6857ce2d0950a5a37a3b8464f2c52f0118e65239
ms2sato <ms2.sato@gmail.com>
2014-08-24 14:44:31 +0900

    コード整形

--- app/controllers/paypal_controller.rb
+++ app/controllers/paypal_controller.rb
@@ -1,19 +1,20 @@
 class PaypalController < ApplicationController
 
-	def register
-	end
-
- 	def view
- 		session[:amount] = params[:amount] 
- 	end
+  def register
+  end
 
   def view
-  	@msg = "こんにちは、世界！"
+    session[:amount] = params[:amount]
+  end
+
+  def view
+    @msg = "こんにちは、世界！"
   end
 
   def return
     logger.debug(request)
   end
+
   def notify
     logger.info(request)
   end
